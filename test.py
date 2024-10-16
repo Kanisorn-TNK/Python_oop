16e4652ef69e487640b659f0b6e7cb2a31926247
Kanisorn_Promjan@tnk <67319010002@tnk.ac.th>
2024-10-16 15:38:25 +0700

    แนะนำตัว
     แนะนำตัวแนะนำตัวแนะนำตัวแนะนำตัวแนะนำตัวแนะนำตัวแนะนำตัวแนะนำตัวแนะนำตัวแนะนำตัวแนะนำตัว

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("YOOOOOOOOOOOOOOOOO");
+print("Hello Python")
+print("ชื่อสกุล : คณิศร พรหมจันทร์")
+print("ชื่อเล่น : กัณต์")
+print("อายุ : 19 ปี")
+print("เลขที่ 2")
+print("รหัสประจำตัวนักศึกษา : 67319010002")
+print("งานอดิเรก : วาดรูป")
